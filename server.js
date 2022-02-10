f5092df0adc3e89e090230dbdeca54553a45ed2d
manutdmohit <saudmohit@gmail.com>
2022-02-10 22:00:07 +0545

    Backend with express

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const PORT = 8000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+// Routing
+app.post('/solve', (req, res) => {
+  const options = {
+    method: 'POST',
+    url: 'https://sudoku-solver2.p.rapidapi.com/',
+    headers: {
+      'content-type': 'application/json',
+      'x-rapidapi-host': 'sudoku-solver2.p.rapidapi.com',
+      'x-rapidapi-key': process.env.RAPID_API_KEY,
+    },
+    data: {
+      input: req.body.numbers,
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      //   console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+app.listen(PORT, console.log(`Server listening on port ${PORT}`));
